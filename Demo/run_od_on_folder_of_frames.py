d4044593bdaba730ce145be52b1c32ab23d19090
ocatias <fabian111@gmx.net>
2021-11-15 11:40:43 +0100

    Refactor

--- Demo/run_od_on_folder_of_frames.py
+++ Demo/run_od_on_folder_of_frames.py
@@ -16,7 +16,7 @@ config_file = "./config/config_vhh_od.yaml"
 
 # If set to true then the script will ask if some of the crops were annotated correctly
 # This allows one to estimate how good the model performs
-do_evaluate_model = False
+do_evaluate_model = True
 
 # Maximum number of images on which the object detection will be run
 # Set to <= 0 if you want to run it on all objects
@@ -29,11 +29,76 @@ n_annotations = 10
 # Set to true to store resulting crops
 do_store_crops = True
 
+def prepare_paths_list(path_list):
+    """
+    Selects n_imgs from the list and shuffles list
+    """
+    global n_imgs
+    paths_chosen = []
+
+    if n_imgs <= 0:
+        paths_chosen = path_list
+
+    while len(path_list) > 0 and n_imgs > 0:
+        n_imgs -= 1
+        i = np.random.randint(0, len(path_list))
+        paths_chosen.append(path_list[i])
+        del path_list[i]
+
+    np.random.shuffle(paths_chosen)
+    return paths_chosen
+
+def store_crops(information, output_folder_path, crop_dict):
+    """
+    Stores crops and updates information string
+    """
+    folder_crop_img_path = os.path.join(output_folder_path, crop_dict["class"])
+    if not os.path.isdir(folder_crop_img_path):
+        os.mkdir(folder_crop_img_path)
+
+    fullpath_crop_img = os.path.join(folder_crop_img_path, crop_dict["name"])
+    cv2.imwrite(fullpath_crop_img, crop_dict["cropped_img"])
+
+    information += ','.join([crop_dict["image_file"], crop_dict["name"], crop_dict["class"], crop_dict["x1"], crop_dict["x2"], crop_dict["y1"], crop_dict["y2"]])
+    information += '\n'
+    return information
+
+def evaluate(crop_dict):
+    """
+    Opens a window and asks the user whether the classification is correct
+    """
+
+    # In case someone touches a wrong key, keep asking for annotations
+    need_to_evaluate = True
+    while(need_to_evaluate):
+        print("\nClass: ", crop_dict["class"])
+        sys.stdout.flush()
+        cv2.imshow(crop_dict["class"], crop_dict["cropped_img"])
+        k = cv2.waitKey(0)
+
+        # There is a bug that waitKey() sometimes gets "ghost" keypresses, if such a keypress occurs call waitKey() again
+        while k == 0:
+            k = cv2.waitKey(0)
+
+        cv2.destroyAllWindows()
+
+        if k==27:       # Esc key to stop
+            need_to_evaluate = False
+            break
+        elif k==49:     # Keyvalue for 1
+            print("Selected: Classification is CORRECT.\n")
+            return True
+        elif k==48:     # Keyvalue for 0
+            print("Selected: Classification is WRONG.\n")
+            return False
+        else:
+            print("Neither 0 or 1 detected, please make sure to not use the numpad.")
+
 def main():
+    np.random.seed(0)
 
     config_instance = Configuration(config_file)
     config_instance.loadConfig()
-
     od_instance = OD(config_file)
 
     #
@@ -60,57 +125,28 @@ def main():
     output_folder_path = args.outpath
 
     #
-    # Main Code
+    # MAIN CODE
     #
 
-    nr_crops = 0
-    nr_correct_crops = 0
+    nr_crops, nr_correct_crops, crops_evaluated = 0, 0, 0
 
-    image_files = os.listdir(folder_path)
-    image_files_full_path = [os.path.join(folder_path, path) for path in image_files]
-
-    if do_evaluate_model:
-        np.random.seed(0)
-
-        # Only use n_imgs images
-        global n_imgs
-        image_files_chosen = []
-
-        if n_imgs <= 0:
-            image_files_chosen = image_files_full_path
-
-        while len(image_files_full_path) > 0 and n_imgs > 0:
-            n_imgs -= 1
-            i = np.random.randint(0, len(image_files_full_path))
-            image_files_chosen.append(image_files_full_path[i])
-            del image_files_full_path[i]
-
-        image_files_full_path = image_files_chosen
-        np.random.shuffle(image_files_full_path)
-        
-        print("Press 1 if the crop and class is correct, 0 if it is wrong. Do not use the numpad. Use escape to quit.")
-        crops_evaluated = 0
+    image_files = [os.path.join(folder_path, path) for path in os.listdir(folder_path)]
+    image_files = prepare_paths_list(image_files)
+    
+    print("Press 1 if the crop and class is correct, 0 if it is wrong. Do not use the numpad. Use escape to quit.")
 
     # Text for the information file
     information = ""
 
-    for crop_dict in od_instance.iterate_over_images(image_files_full_path):
+    for crop_dict in od_instance.iterate_over_images(image_files):
         nr_crops += 1
 
-        # Store crops
+        # Store crops and update information string
         if do_store_crops:
-            folder_crop_img_path = os.path.join(output_folder_path, crop_dict["class"])
-            if not os.path.isdir(folder_crop_img_path):
-                os.mkdir(folder_crop_img_path)
-
-            fullpath_crop_img = os.path.join(folder_crop_img_path, crop_dict["name"])
-            cv2.imwrite(fullpath_crop_img, crop_dict["cropped_img"])
-
-            information += ','.join([crop_dict["image_file"], crop_dict["name"], crop_dict["class"], crop_dict["x1"], crop_dict["x2"], crop_dict["y1"], crop_dict["y2"]])
-            information += '\n'
+            information = store_crops(information, output_folder_path, crop_dict)
 
         # Check if crop is correct
-        if do_evaluate_model and  np.random.random() < float(n_annotations) / len(image_files_full_path):
+        if do_evaluate_model and  np.random.random() < float(n_annotations) / len(image_files):
             if crops_evaluated >= n_annotations:
                 continue
 
@@ -118,42 +154,19 @@ def main():
             if crop_dict["class"] == "tie":
                 continue
 
-            need_to_evaluate = True
             crops_evaluated += 1
+            is_correct = evaluate(crop_dict)
 
-            # In case someone touches a wrong key, keep asking for annotations
-            while(need_to_evaluate):
-                print("\nClass: ", crop_dict["class"])
-                sys.stdout.flush()
-                cv2.imshow(crop_dict["class"], crop_dict["cropped_img"])
-                k = cv2.waitKey(0)
+            if is_correct:
+                nr_correct_crops += 1
 
-                # There is a bug that waitKey() sometimes gets "ghost" keypresses, if such a keypress occurs call waitKey() again
-                while k == 0:
-                    k = cv2.waitKey(0)
-
-                cv2.destroyAllWindows()
-
-                if k==27:       # Esc key to stop
-                    need_to_evaluate = False
-                    break
-                elif k==49:     # Keyvalue for 1
-                    print("Correct crop.\n")
-                    nr_correct_crops += 1
-                    need_to_evaluate = False
-                elif k==48:     # Keyvalue for 0
-                    print("Wrong crop.\n")
-                    need_to_evaluate = False
-                else:
-                    print("Neither 0 or 1 detected, please make sure to not use the numpad.")
             print("Annotated {0} / {1}".format(crops_evaluated, n_annotations))               
 
-    # Create information file        
+    # Store information string in file        
     if do_store_crops:
         information_file = open(os.path.join(output_folder_path, "crop_information.txt"), "w")
         information_file.write("original_image, crop_image, class_name, x1, x2, y1, y2\n")
         information_file.write(information)
-
         information_file.close()
 
     # Output data
